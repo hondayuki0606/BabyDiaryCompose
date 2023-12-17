dab6f6f15cf4756ec7c846ebb0e3650c2131c0ac
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2023-12-17 20:52:28 +0900

    BDT-3, ViewModelにasStateFlowを実装

--- app/src/main/java/com/example/babydiarycompose/viewmodel/StateViewModel.kt
+++ app/src/main/java/com/example/babydiarycompose/viewmodel/StateViewModel.kt
@@ -5,13 +5,21 @@ import com.example.babydiarycompose.data.ActionItem
 import com.example.babydiarycompose.data.SessionDetailState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import java.time.LocalDateTime
 import javax.inject.Inject
 
 @HiltViewModel
 class StateViewModel @Inject constructor() : ViewModel() {
+    private val _uiState = MutableStateFlow(
+        SessionDetailState(
+            ActionItem(LocalDateTime.now(), null, "name", "detail")
+        )
+    )
 
-    //    var state = Flow<SessionDetailStateState>
+    val uiState = _uiState.asStateFlow()
     suspend fun getState(): SessionDetailState {
         coroutineScope {
         }
