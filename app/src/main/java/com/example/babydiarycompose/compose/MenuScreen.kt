d0e94d245076949cec51988abc50401782bcd8a8
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2023-12-23 21:11:30 +0900

    BDT-11, メニュー画面の枠を作成

---
+++ app/src/main/java/com/example/babydiarycompose/compose/MenuScreen.kt
@@ -0,0 +1,26 @@
+package com.example.babydiarycompose.compose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MenuScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray)
+            .size(100.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+       Text(text = "settings")
+    }
+}
