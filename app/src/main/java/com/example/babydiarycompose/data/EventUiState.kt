f9c9fbde13a2b5e3b3f221f28c0b428e973ff1e4
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-01-27 21:37:45 +0900

    BDT-54, データクラスにStableアノテーションを追加して再描写しないよう対応。ソース整理

--- app/src/main/java/com/example/babydiarycompose/data/EventUiState.kt
+++ app/src/main/java/com/example/babydiarycompose/data/EventUiState.kt
@@ -1,5 +1,8 @@
 package com.example.babydiarycompose.data
 
+import androidx.compose.runtime.Stable
+
+@Stable
 data class EventUiState(
     val eventList: List<Event>,
     val iconList: List<Icon>
--- app/src/main/java/com/example/babydiarycompose/repository/EventRepositoryImpl.kt
+++ app/src/main/java/com/example/babydiarycompose/repository/EventRepositoryImpl.kt
@@ -10,20 +10,6 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class EventRepositoryImpl @Inject constructor() : EventRepository {
-    override suspend fun getEventList(applicationContext: Context): List<Event> {
-        val result = arrayListOf<Event>()
-        CoroutineScope(Dispatchers.IO).launch {
-            val db = AppDatabase.getDatabase(applicationContext)
-            val eventDao = db.eventDao()
-            val eventList = eventDao.getAll()
-            eventList.forEach {
-                result.add(Event(it.time ?: "", it.icon ?: 0, it.eventName ?: "", ""))
-            }
-
-        }.join()
-        return result
-    }
-
     override suspend fun addEventList(
         applicationContext: Context,
         eventList: List<Event>
@@ -44,6 +30,31 @@ class EventRepositoryImpl @Inject constructor() : EventRepository {
                 )
             }
             result = true
+
+        }.join()
+        return result
+    }
+
+    override suspend fun getEventList(applicationContext: Context): List<Event> {
+        val result = arrayListOf<Event>()
+        CoroutineScope(Dispatchers.IO).launch {
+            val db = AppDatabase.getDatabase(applicationContext)
+            val eventDao = db.eventDao()
+            // データ生成
+//            for (i in 0..3) {
+//                eventDao.insertAll(
+//                    com.example.babydiarycompose.model.Event(
+//                        i,
+//                        "${i}:00",
+//                        R.drawable.milk_icon,
+//                        "ミルク"
+//                    ),
+//                )
+//            }
+            val eventList = eventDao.getAll()
+            eventList.forEach {
+                result.add(Event(it.time ?: "", it.icon ?: 0, it.eventName ?: "", ""))
+            }
 
         }.join()
         return result
