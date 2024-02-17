b8107006c50e6e2c99d38d21f9cf1f231fab3e59
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-02-17 22:17:12 +0900

    BDT-78, where NumberとComparableについて調査し、理解できたのでコメント追加

--- app/src/main/java/com/example/babydiarycompose/data/Datum.kt
+++ app/src/main/java/com/example/babydiarycompose/data/Datum.kt
@@ -1,6 +1,9 @@
 package com.example.babydiarycompose.data
 
 data class Datum<T>(val value: List<T>, val label: String)
-        where T : Number, T : Comparable<T>
+// where　T:NumberにするとTには数値しか入れれなくなる、
+// where T:Comparable<T>にするとTは同じ型しか入れれなくなるInt,Floatと別々の引数はいれることができない
+// 制約をかける必要がないのでコメントアウトする
+//        where T : Number, T : Comparable<T>
 
 data class Item(val event:String,val value:Int)
