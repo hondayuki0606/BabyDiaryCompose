eb3673adf49a9a5c444190868ae7bc1ce9e0e2bf
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-02-22 21:33:40 +0900

    BDT-85, 成長の体重、身長のデータクラス作成

--- app/src/main/java/com/example/babydiarycompose/data/GrowthCurveUiState.kt
+++ app/src/main/java/com/example/babydiarycompose/data/GrowthCurveUiState.kt
@@ -4,7 +4,7 @@ data class GrowthCurveUiState(
     val ageList: List<String>,
     val weightList: List<String>,
     val cmList: List<String>,
-    val wightList:List<GrowthWeightData>,
-    val heightList:List<GrowthHeightData>,
+    val growthWeight:List<GrowthWeightData>,
+    val growthHeight:List<GrowthHeightData>,
     val tabList: List<String>
 )
--- app/src/main/java/com/example/babydiarycompose/viewmodel/GrowthCurveViewModel.kt
+++ app/src/main/java/com/example/babydiarycompose/viewmodel/GrowthCurveViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.babydiarycompose.data.Event
 import com.example.babydiarycompose.data.GrowthCurveUiState
-import com.example.babydiarycompose.data.GrowthData
 import com.example.babydiarycompose.repository.EventRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -24,7 +23,8 @@ class GrowthCurveViewModel @Inject constructor(
             ageList = arrayListOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"),
             weightList = arrayListOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"),
             cmList = arrayListOf("40", "45", "50", "55", "60", "65", "70", "75", "80"),
-            recordList = listOf(),
+            growthWeight = listOf(),
+            growthHeight = listOf(),
             tabList = arrayListOf("食事", "睡眠", "排泄", "体温", "日記", "すべて")
         )
     )
@@ -43,18 +43,10 @@ class GrowthCurveViewModel @Inject constructor(
     fun getEventList(appContext: Context) {
         viewModelScope.launch(Dispatchers.IO) {
             eventRepository.getEventList(appContext).let { eventList ->
-                val tmp = mutableListOf(
-                    GrowthData(1, 12),
-                    GrowthData(2, 13),
-                    GrowthData(3, 15),
-                    GrowthData(4, 18),
-                    GrowthData(5, 19),
-                    GrowthData(6, 21),
-                    GrowthData(7, 24),
-                )
+
                 _uiState.update {
                     it.copy(
-                        recordList = tmp
+
                     )
                 }
             }
