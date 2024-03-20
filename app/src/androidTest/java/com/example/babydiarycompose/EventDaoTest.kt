b2a1004aa96daf8f11bcabdc97fa831ad75967d8
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-03-20 23:44:58 +0900

    BDT-122, Daoテストケース記載

--- app/src/androidTest/java/com/example/babydiarycompose/EventDaoTest.kt
+++ app/src/androidTest/java/com/example/babydiarycompose/EventDaoTest.kt
@@ -5,9 +5,10 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import com.example.babydiarycompose.dao.EventDao
 import com.example.babydiarycompose.database.AppDatabase
-import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
+import com.example.babydiarycompose.model.Event
+import org.junit.After
 
 class EventDaoTest {
     private lateinit var dataBase: AppDatabase
@@ -16,15 +17,29 @@ class EventDaoTest {
     @Before
     fun createDb() {
         val context = ApplicationProvider.getApplicationContext<Context>()
-        dataBase = Room.inMemoryDatabaseBuilder(
-            context,
-            AppDatabase::class.java,
-        ).build()
+        dataBase = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).build()
         eventDao = dataBase.eventDao()
     }
 
+    @After
+    fun closeDb() {
+        dataBase.close()
+    }
+
     @Test
-    fun test_dummy() = runTest {
-//        assertThat(1,1)
+    fun test_dummy() {
+        // prepare
+        val event = Event(0, "2024/03/12", 1, "母乳", "5分")
+        eventDao.insertAll(event)
+
+        // execute
+        val eventValue = eventDao.getAll()
+
+        // verify
+        assert(eventValue.size == 1)
+        assert(eventValue.first().time == "2024/03/12")
+        assert(eventValue.first().icon == 1)
+        assert(eventValue.first().eventName == "母乳")
+        assert(eventValue.first().eventDetail == "5分")
     }
 }
