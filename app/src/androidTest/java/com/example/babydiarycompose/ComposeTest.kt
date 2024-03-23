eb965a1eaaa54343a403f4b95a2a114a2db2cfca
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-03-24 00:14:08 +0900

    BDT-125, ComposeのHiltAndroidTestアノテーションをコメントアウトすると他のテストは通ることを確認

--- app/src/androidTest/java/com/example/babydiarycompose/ComposeTest.kt
+++ app/src/androidTest/java/com/example/babydiarycompose/ComposeTest.kt
@@ -1,15 +1,25 @@
 package com.example.babydiarycompose
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-@HiltAndroidTest
+//@HiltAndroidTest
+//@Config(application = HiltTestApplication::class)
+@RunWith(AndroidJUnit4::class)
 class ComposeTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeTestRule = createComposeRule()
+
     @Before
     fun init() {
         hiltRule.inject()
