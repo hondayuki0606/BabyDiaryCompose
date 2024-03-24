bd1a0eddad635471e7e224017684a2bb30145711
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-03-25 00:07:12 +0900

    BDT-126, hiltを2.48から2.44に変更

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+//buildscript {
+//    repositories {
+//        mavenCentral()
+//        google()
+//    }
+//    dependencies {
+//        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
+//        // Test
+//        classpath("junit:junit:4.13.2")
+//    }
+//}
+
 plugins {
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
-    id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
 }
