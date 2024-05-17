5c434762d3ac25b7851d71439481c066af4012e0
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-05-17 23:23:08 +0900

    BDT-183, EventTimeDialogをConstraintLayoutで実装

--- app/src/main/java/com/example/babydiarycompose/ui/dialog/EventTimeSettingDialog.kt
+++ app/src/main/java/com/example/babydiarycompose/ui/dialog/EventTimeSettingDialog.kt
@@ -1,11 +1,13 @@
 package com.example.babydiarycompose.ui.dialog
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -32,10 +34,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.babydiarycompose.compose.NumberPicker
 import com.example.babydiarycompose.data.Event
 import com.example.babydiarycompose.ui.theme.BabyDiaryComposeTheme
+import com.example.babydiarycompose.ui.theme.DialogBackDark
 import com.example.babydiarycompose.ui.theme.DialogBackGray
 import com.example.babydiarycompose.ui.theme.Pink
 import com.example.babydiarycompose.viewmodel.RecordingViewModel
@@ -93,134 +97,149 @@ fun EventTimeSettingDialog(
     Dialog(onDismissRequest = { setShowDialog(false) }) {
         Surface(
             shape = RoundedCornerShape(16.dp),
-            color = Color.DarkGray
+            color = Color(0x00000000),
         ) {
-            Box(
-                contentAlignment = Alignment.Center
+            ConstraintLayout(
+                modifier = Modifier
+                    .background(Color(0x00000000))
+                    .fillMaxSize()
             ) {
-                Column(modifier = Modifier.padding(20.dp)) {
-
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Image(
-                            modifier = Modifier
-                                .padding(
-                                    start = 10.dp,
-                                    end = 10.dp,
-                                ),
-                            contentScale = ContentScale.Fit,
-                            painter = painterResource(resIcon),
-                            contentDescription = "image"
-                        )
-                        Text(
-                            text = eventName,
-                            style = TextStyle(
-                                fontSize = 24.sp,
-                                fontWeight = FontWeight.Bold
+                val (eventTitle, picker, buttonArea) = createRefs()
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(DialogBackGray)
+                        .constrainAs(eventTitle) {
+                            top.linkTo(parent.top)
+                            bottom.linkTo(picker.top)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                        },
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .padding(
+                                start = 10.dp,
+                                end = 10.dp,
                             ),
-                            color = Color.White
-                        )
-                        Image(
-                            modifier = Modifier
-                                .padding(
-                                    start = 10.dp,
-                                    end = 10.dp,
-                                ),
-                            contentScale = ContentScale.Fit,
-                            painter = painterResource(resIcon),
-                            contentDescription = "image"
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.height(20.dp))
-
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        NumberPicker(
-                            state = hourState,
-                            range = 0..23,
-                        )
-                        Spacer(modifier = Modifier.width(20.dp))
-                        NumberPicker(
-                            state = minutesState,
-                            range = 0..59,
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.height(20.dp))
-                    val scope = rememberCoroutineScope()
-                    Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
-                        Column {
-                            Button(
-                                onClick = {
-                                    scope.launch {
-                                        when (eventName) {
-                                            "母乳" -> {
-                                                showBreastMilkDialog.value = true
-                                            }
-
-                                            "ミルク" -> {
-                                                showMilkDialog.value = true
-                                            }
-
-                                            else -> {
-                                                val eventList = arrayListOf(
-                                                    Event(
-                                                        yearAndMonthAndDay = selectedDate,
-                                                        "${hourState.intValue}:${
-                                                            String.format(
-                                                                "%02d",
-                                                                minutesState.intValue
-                                                            )
-                                                        }",
-                                                        resIcon,
-                                                        eventName,
-                                                        ""
-                                                    )
-                                                )
-                                                viewModel.addEventList(
-                                                    applicationContext,
-                                                    eventList
-                                                )
-                                                setShowDialog(false)
-                                            }
-                                        }
+                        contentScale = ContentScale.Fit,
+                        painter = painterResource(resIcon),
+                        contentDescription = "image"
+                    )
+                    Text(
+                        text = eventName,
+                        style = TextStyle(
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold
+                        ),
+                        color = Color.White
+                    )
+                    Image(
+                        modifier = Modifier
+                            .padding(
+                                start = 10.dp,
+                                end = 10.dp,
+                            ),
+                        contentScale = ContentScale.Fit,
+                        painter = painterResource(resIcon),
+                        contentDescription = "image"
+                    )
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(DialogBackGray)
+                        .constrainAs(picker) {
+                            top.linkTo(eventTitle.bottom)
+                            bottom.linkTo(buttonArea.top)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                        },
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    NumberPicker(
+                        state = hourState,
+                        range = 0..23,
+                    )
+                    Spacer(modifier = Modifier.width(20.dp))
+                    NumberPicker(
+                        state = minutesState,
+                        range = 0..59,
+                    )
+                }
+                val scope = rememberCoroutineScope()
+                Column(modifier = Modifier
+                    .constrainAs(buttonArea) {
+                        top.linkTo(picker.bottom)
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }) {
+                    Button(
+                        onClick = {
+                            scope.launch {
+                                when (eventName) {
+                                    "母乳" -> {
+                                        showBreastMilkDialog.value = true
                                     }
 
-                                },
-                                shape = RoundedCornerShape(50.dp),
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .height(50.dp),
-                                colors = ButtonDefaults.textButtonColors(
-                                    containerColor = DialogBackGray,
-                                    contentColor = DialogBackGray
-                                )
-                            ) {
-                                Text(text = "OK", color = Pink)
+                                    "ミルク" -> {
+                                        showMilkDialog.value = true
+                                    }
+
+                                    else -> {
+                                        val eventList = arrayListOf(
+                                            Event(
+                                                yearAndMonthAndDay = selectedDate,
+                                                "${hourState.intValue}:${
+                                                    String.format(
+                                                        "%02d",
+                                                        minutesState.intValue
+                                                    )
+                                                }",
+                                                resIcon,
+                                                eventName,
+                                                ""
+                                            )
+                                        )
+                                        viewModel.addEventList(
+                                            applicationContext,
+                                            eventList
+                                        )
+                                        setShowDialog(false)
+                                    }
+                                }
                             }
-                            Spacer(modifier = Modifier.height(20.dp))
-                            Button(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .height(50.dp),
-                                onClick = {
-                                    setShowDialog(false)
-                                },
-                                colors = ButtonDefaults.textButtonColors(
-                                    containerColor = DialogBackGray,
-                                    contentColor = DialogBackGray
-                                )
-                            ) {
-                                Text(text = "キャンセル", color = Pink)
-                            }
-                        }
+
+                        },
+                        shape = RoundedCornerShape(50.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp),
+                        colors = ButtonDefaults.textButtonColors(
+                            containerColor = DialogBackGray,
+                            contentColor = DialogBackGray
+                        )
+                    ) {
+                        Text(text = "OK", color = Pink)
+                    }
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Button(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp),
+                        onClick = {
+                            setShowDialog(false)
+                        },
+                        colors = ButtonDefaults.textButtonColors(
+                            containerColor = DialogBackGray,
+                            contentColor = DialogBackGray
+                        )
+                    ) {
+                        Text(text = "キャンセル", color = Pink)
                     }
                 }
             }
