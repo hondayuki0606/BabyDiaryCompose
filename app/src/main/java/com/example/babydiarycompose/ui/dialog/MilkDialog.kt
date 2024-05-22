abeabe7d9cfd5eebe979192350217f6640be0f26
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-05-22 22:58:18 +0900

    BDT-188, ミルクダイアログを修正

--- app/src/main/java/com/example/babydiarycompose/ui/dialog/MilkDialog.kt
+++ app/src/main/java/com/example/babydiarycompose/ui/dialog/MilkDialog.kt
@@ -1,11 +1,14 @@
 package com.example.babydiarycompose.ui.dialog
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,6 +16,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,14 +26,23 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.babydiarycompose.data.Event
 import com.example.babydiarycompose.ui.theme.BabyDiaryComposeTheme
+import com.example.babydiarycompose.ui.theme.DialogBackDark
+import com.example.babydiarycompose.ui.theme.DialogBackGray
+import com.example.babydiarycompose.ui.theme.Pink
 import com.example.babydiarycompose.viewmodel.RecordingViewModel
 import kotlinx.coroutines.launch
 
@@ -47,20 +60,63 @@ fun MilkDialog(
     val applicationContext = LocalContext.current
     Dialog(onDismissRequest = { setShowDialog(false) }) {
         Surface(
-            shape = RoundedCornerShape(16.dp), color = Color.DarkGray
+            color = Color(0x00000000),
+            shape = RoundedCornerShape(4.dp),
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
         ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally
+            ConstraintLayout(
+                modifier = Modifier
+                    .background(Color(0x00000000))
+                    .fillMaxSize()
             ) {
-                val times = arrayListOf("10ml", "20ml", "30ml")
+                val (milkTitle, mlList, buttonArea) = createRefs()
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 12.dp)
+                        .background(DialogBackDark)
+                        .constrainAs(milkTitle) {
+                            top.linkTo(parent.top)
+                            bottom.linkTo(mlList.top)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                        }
+                ) {
+                    Text(
+                        text = "ミルク",
+                        modifier = Modifier.padding(top = 5.dp, bottom = 5.dp),
+                        color = Color.White,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+                val mls = arrayListOf<String>()
+                for (ml in 0..15 step 5) {
+                    mls.add("${ml}ml")
+                }
+                for (ml in 20..500 step 10) {
+                    mls.add("${ml}ml")
+                }
                 val scope = rememberCoroutineScope()
                 LazyColumn(
-                    modifier = Modifier.height(100.dp),
+                    modifier = Modifier
+                        .padding(bottom = 12.dp)
+                        .background(DialogBackDark)
+                        .constrainAs(mlList) {
+                            top.linkTo(milkTitle.bottom)
+                            bottom.linkTo(buttonArea.top)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            height = Dimension.fillToConstraints
+                            width = Dimension.fillToConstraints
+                        },
                     verticalArrangement = Arrangement.SpaceAround
                 ) {
-                    items(times) {
+                    items(mls) {
                         Text(text = it,
-                            color = Color.Black,
+                            color = Color.White,
                             fontSize = 12.sp,
                             modifier = Modifier.clickable {
                                 scope.launch {
@@ -77,27 +133,40 @@ fun MilkDialog(
                                     setShowDialog(false)
                                     resultValue(true)
                                 }
-                            })
+                            }
+                        )
                     }
                 }
-                Spacer(modifier = Modifier.height(20.dp))
-
-                Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
-                    Column {
-                        Button(
-                            onClick = {
-                                resultValue(false)
-                            },
-                            shape = RoundedCornerShape(50.dp),
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(50.dp)
-                        ) {
-                            Text(text = "キャンセル", modifier = Modifier.clickable {
+                Box(
+                    modifier = Modifier
+                        .padding(40.dp, 0.dp, 40.dp, 0.dp)
+                        .constrainAs(buttonArea) {
+                            top.linkTo(mlList.bottom)
+                            bottom.linkTo(parent.bottom)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                        },
+                ) {
+                    Button(
+                        onClick = {
+                            resultValue(false)
+                        },
+                        colors = ButtonDefaults.textButtonColors(
+                            containerColor = DialogBackGray,
+                            contentColor = DialogBackGray
+                        ),
+                        shape = RoundedCornerShape(50.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                    ) {
+                        Text(text = "キャンセル",
+                            color = Pink,
+                            modifier = Modifier.clickable {
                                 setShowDialog(false)
                                 resultValue(true)
-                            })
-                        }
+                            }
+                        )
                     }
                 }
             }
