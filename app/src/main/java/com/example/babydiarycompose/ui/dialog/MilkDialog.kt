ec6cc27bc3079733c441a4719dc021887be3c41b
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-05-26 23:23:35 +0900

    BDT-193. ミルクダイアログのmlのstartのpadding,クリックの範囲を修正

--- app/src/main/java/com/example/babydiarycompose/ui/dialog/MilkDialog.kt
+++ app/src/main/java/com/example/babydiarycompose/ui/dialog/MilkDialog.kt
@@ -1,18 +1,14 @@
 package com.example.babydiarycompose.ui.dialog
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -28,10 +24,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,7 +35,6 @@ import androidx.constraintlayout.compose.Dimension
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.babydiarycompose.data.Event
 import com.example.babydiarycompose.ui.theme.BabyDiaryComposeTheme
-import com.example.babydiarycompose.ui.theme.DialogBackDark
 import com.example.babydiarycompose.ui.theme.DialogBackGray
 import com.example.babydiarycompose.ui.theme.Pink
 import com.example.babydiarycompose.viewmodel.RecordingViewModel
@@ -115,12 +107,11 @@ fun MilkDialog(
                             width = Dimension.fillToConstraints
                         },
                 ) {
-                    items(mls) {
-                        Text(text = it,
-                            color = Color.White,
-                            fontSize = 14.sp,
-                            modifier = Modifier.height(40.dp)
-                                .wrapContentSize(Alignment.CenterStart)
+                    items(mls) { ml ->
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 5.dp)
                                 .clickable {
                                     scope.launch {
                                         val eventList = arrayListOf(
@@ -129,7 +120,7 @@ fun MilkDialog(
                                                 "$hour:${String.format("%02d", minutes)}",
                                                 resIcon,
                                                 eventName,
-                                                it
+                                                ml
                                             )
                                         )
                                         viewModel.addEventList(applicationContext, eventList)
@@ -137,12 +128,22 @@ fun MilkDialog(
                                         resultValue(true)
                                     }
                                 }
-                        )
-                        Box(
-                            Modifier
-                                .fillMaxWidth()
-                                .height(2.dp)
-                                .background(color = Color.Gray))
+                        ) {
+                            Text(text = ml,
+                                color = Color.White,
+                                fontSize = 14.sp,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(40.dp)
+                                    .wrapContentSize(Alignment.CenterStart)
+                            )
+                            Box(
+                                Modifier
+                                    .fillMaxWidth()
+                                    .height(2.dp)
+                                    .background(color = Color.Gray)
+                            )
+                        }
                     }
                 }
                 Box(
