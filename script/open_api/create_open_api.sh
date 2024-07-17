2eb1f065bf66d6b1cc92df28412328d22f92efcc
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-07-17 21:39:45 +0900

    BDT-244, openApiGeneratorを実行するshを修正。仮で生成したAPIとModelを配置

--- script/open_api/create_open_api.sh
+++ script/open_api/create_open_api.sh
@@ -1,2 +1,4 @@
 java -jar openapi-generator-cli.jar generate -g kotlin --additional-properties=prependFormOrBodyParameters=true -o out -i yaml/test.yaml
+cp -r out/src/main/kotlin/org/openapitools/client/apis ../../app/src/main/java/com/example/babydiarycompose/
+cp -r out/src/main/kotlin/org/openapitools/client/models ../../app/src/main/java/com/example/babydiarycompose/
 rm -r ./out
