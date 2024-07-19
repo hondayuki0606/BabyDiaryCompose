c61eb6745cc056a8d79b1d54af7c63c2eef8ab21
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-07-19 22:24:54 +0900

    BDT-246, open-api-generator package指定

--- script/open_api/create_open_api.sh
+++ script/open_api/create_open_api.sh
@@ -1,6 +1,18 @@
 #java -jar openapi-generator-cli.jar generate -g kotlin --additional-properties=prependFormOrBodyParameters=true -o out -i yaml/pet.yaml
 #java -jar openapi-generator-cli.jar generate -g kotlin --additional-properties=prependFormOrBodyParameters=true -o out -i yaml/store.yaml
-java -jar openapi-generator-cli.jar generate -g kotlin --additional-properties=prependFormOrBodyParameters=true -o out -i yaml/user.yaml
-cp -r out/src/main/kotlin/org/openapitools/client/apis ../../app/src/main/java/com/example/babydiarycompose/
-cp -r out/src/main/kotlin/org/openapitools/client/models ../../app/src/main/java/com/example/babydiarycompose/
+#java -jar openapi-generator-cli.jar generate -g kotlin --additional-properties=prependFormOrBodyParameters=true -o out -i 
+java -jar openapi-generator-cli.jar generate -i yaml/user.yaml -g kotlin -o out --group-id com.example --api-package com.example.babydiarycompose.api --model-package com.example.babydiarycompose.models
+
+#java -jar openapi-generator-cli.jar generate \
+#  -i yaml/user.yaml  \
+#  -g kotlin \
+#  -o out \
+#  -c ./openapi.config \
+#  --group-id com.example  \
+#  --artifact-id sample-api-generated  \
+#  --artifact-version 0.0.1-SNAPSHOT  \
+#  --api-package com.example.api.controller  \
+#  --model-package com.example.api.model
+cp -r out/src/main/kotlin/com/example/babydiarycompose/api ../../app/src/main/java/com/example/babydiarycompose/
+cp -r out/src/main/kotlin/com/example/babydiarycompose/models ../../app/src/main/java/com/example/babydiarycompose/
 rm -r ./out
