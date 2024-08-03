5e0f934f56ebb49743728ecea38ef1f54bc0f473
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-08-03 21:18:16 +0900

    BDT-259, api-Moduleを作成

--- app/src/main/java/com/example/babydiarycompose/di/ApiModule.kt
+++ app/src/main/java/com/example/babydiarycompose/di/ApiModule.kt
@@ -1,5 +1,7 @@
 package com.example.babydiarycompose.di
 
+import com.example.babydiarycompose.api.UserApi
+import com.example.babydiarycompose.infrastructure.ApiClient
 import com.example.babydiarycompose.repository.EventRepository
 import com.example.babydiarycompose.repository.EventRepositoryImpl
 import dagger.Module
@@ -7,9 +9,11 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
+
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
+object ApiModule {
+
     @Provides
-    fun provideEventRepository(): EventRepository = EventRepositoryImpl()
+    fun provideUserApi(client: ApiClient): UserApi = client.createService(UserApi::class.java)
 }
--- script/open_api/create_open_api.sh
+++ script/open_api/create_open_api.sh
@@ -9,7 +9,7 @@ java -jar openapi-generator-cli.jar generate \
     --api-package com.example.babydiarycompose.api \
     --invoker-package com.example.babydiarycompose.invoker \
     --model-package com.example.babydiarycompose.models \
-#    --additional-properties collectionType=list,dateLibrary=java8,enumPropertyNaming=UPPERCASE,serializationLibrary=kotlinx_serialization,useCoroutines=true
+    --additional-properties collectionType=list,dateLibrary=java8,enumPropertyNaming=UPPERCASE,serializationLibrary=kotlinx_serialization,useCoroutines=true
 cp -r out/src/main/kotlin/com/example/babydiarycompose/api ../../app/src/main/java/com/example/babydiarycompose/
 cp -r out/src/main/kotlin/com/example/babydiarycompose/models ../../app/src/main/java/com/example/babydiarycompose/
 cp -r out/src/main/kotlin/com/example/babydiarycompose/infrastructure ../../app/src/main/java/com/example/babydiarycompose/
