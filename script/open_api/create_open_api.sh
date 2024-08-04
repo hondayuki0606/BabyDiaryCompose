2f693c5384492ac9a17de94fab146a58d08697f8
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-08-04 22:06:16 +0900

    BDT-260, open-api-Generatorのオプションについて調査、コメント記載

--- script/open_api/create_open_api.sh
+++ script/open_api/create_open_api.sh
@@ -10,6 +10,10 @@ java -jar openapi-generator-cli.jar generate \
     --invoker-package com.example.babydiarycompose.invoker \
     --model-package com.example.babydiarycompose.models \
     --additional-properties collectionType=list,dateLibrary=java8,enumPropertyNaming=UPPERCASE,serializationLibrary=kotlinx_serialization,useCoroutines=true
+    #useCoroutines=true はsuspend付与
+    #dateLibrary=java8 は変化なし
+    #enumPropertyNaming=UPPERCASE は変化なし
+    #serializationLibrary=kotlinx_serialization は@SerializableをつけることができJsonのエンコード、デコードが容易にできるようになる
 cp -r out/src/main/kotlin/com/example/babydiarycompose/api ../../app/src/main/java/com/example/babydiarycompose/
 cp -r out/src/main/kotlin/com/example/babydiarycompose/models ../../app/src/main/java/com/example/babydiarycompose/
 cp -r out/src/main/kotlin/com/example/babydiarycompose/infrastructure ../../app/src/main/java/com/example/babydiarycompose/
