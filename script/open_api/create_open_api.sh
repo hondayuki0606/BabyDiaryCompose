d203fcb6408d2212ad9407d042f0c41f0f6cd1bc
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-08-12 21:52:33 +0900

    BDT-271, scriptの実行コマンドをechoで表示

--- script/open_api/create_open_api.sh
+++ script/open_api/create_open_api.sh
@@ -6,11 +6,19 @@ BABY_DIARY_COMPOSE_PACKAGE=com.example.babydiarycompose
 
 # プロジェクト内の API Client のディレクトリ
 API_CLIENT_DIR=$SCRIPT_DIR/apiclient/src/main/kotlin/dev/honwakalab/linkmark/apiclient
+echo java -jar openapi-generator-cli-7.0.1.jar generate
+echo --input-spec yaml/user.yaml
+echo --library jvm-retrofit2
+echo --generator-name kotlin
+echo --output ./out
+echo --package-name com.example.babydiarycompose
+echo --group-id com.example
 echo --package-name $BABY_DIARY_COMPOSE_PACKAGE
 echo --api-package $BABY_DIARY_COMPOSE_PACKAGE.api
 echo --invoker-package $BABY_DIARY_COMPOSE_PACKAGE.invoker
 echo --model-package $BABY_DIARY_COMPOSE_PACKAGE.models
-java -jar openapi-generator-cli.jar generate \
+echo --additional-properties collectionType=list,dateLibrary=java8,enumPropertyNaming=UPPERCASE,serializationLibrary=kotlinx_serialization,useCoroutines=true
+java -jar openapi-generator-cli-7.0.1.jar generate \
     --input-spec yaml/user.yaml \
     --library jvm-retrofit2 \
     --generator-name kotlin \
@@ -29,5 +37,4 @@ java -jar openapi-generator-cli.jar generate \
 cp -r out/src/main/kotlin/com/example/babydiarycompose/api ../../app/src/main/java/com/example/babydiarycompose/
 cp -r out/src/main/kotlin/com/example/babydiarycompose/models ../../app/src/main/java/com/example/babydiarycompose/
 cp -r out/src/main/kotlin/com/example/babydiarycompose/infrastructure ../../app/src/main/java/com/example/babydiarycompose/
-cp -r out/src/main/kotlin/com/example/babydiarycompose/auth ../../app/src/main/java/com/example/babydiarycompose/
 rm -r ./out
