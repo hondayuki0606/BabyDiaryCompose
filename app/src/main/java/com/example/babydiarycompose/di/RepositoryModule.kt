19e773bd37c9aad7ef6c749d824b5c60521cb98f
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-08-12 21:49:00 +0900

    BDT-270, RepositoryModuleにUserとPostsを追加

--- app/src/main/java/com/example/babydiarycompose/di/RepositoryModule.kt
+++ app/src/main/java/com/example/babydiarycompose/di/RepositoryModule.kt
@@ -1,7 +1,13 @@
 package com.example.babydiarycompose.di
 
+import com.example.babydiarycompose.api.PostsApi
+import com.example.babydiarycompose.api.UserApi
 import com.example.babydiarycompose.repository.EventRepository
 import com.example.babydiarycompose.repository.EventRepositoryImpl
+import com.example.babydiarycompose.repository.PostsRepository
+import com.example.babydiarycompose.repository.PostsRepositoryImpl
+import com.example.babydiarycompose.repository.UserRepository
+import com.example.babydiarycompose.repository.UserRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,4 +18,14 @@ import dagger.hilt.components.SingletonComponent
 object RepositoryModule {
     @Provides
     fun provideEventRepository(): EventRepository = EventRepositoryImpl()
+
+    @Provides
+    fun provideUserRepository(userApi: UserApi): UserRepository {
+        return UserRepositoryImpl(userApi)
+    }
+
+    @Provides
+    fun providePostsRepository(postsApi: PostsApi): PostsRepository {
+        return PostsRepositoryImpl(postsApi)
+    }
 }
