032e4c3d71c69c282cd4ed37c943db5f9a20d349
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-08-17 22:04:31 +0900

    BDT-276, サーバーレスポンスが返ってくるjsを検討中

--- test-server/server.js
+++ test-server/server.js
@@ -32,21 +32,36 @@
 
 // Your code here!
 
-var http = require('http');
-var server = http.createServer();
+//var http = require('http');
+//var server = http.createServer();
+//
+//var fs = require('fs');
+//var xml_string;
+//
+//server.on('request', function (req, res) {
+//    if (req.url.indexOf('TestAPI') != -1) {
+//        fs.readFile('./routeSearch/stub_200_routesearch_response_body_10.xml', 'utf-8', (err, files) => {
+//            if (err) {throw err;}
+//            xml_string = files;
+//            res.writeHead(200,{'Content-Type':'application/xml'});
+//            res.end(xml_string);
+//        });
+//    }
+//});
 
-var fs = require('fs');
-var xml_string;
+// パッケージを読み込む
+let http = require('http');
 
-server.on('request', function (req, res) {
-    if (req.url.indexOf('TestAPI') != -1) {
-        fs.readFile('./routeSearch/stub_200_routesearch_response_body_10.xml', 'utf-8', (err, files) => {
-            if (err) {throw err;}
-            xml_string = files;
-            res.writeHead(200,{'Content-Type':'application/xml'});
-            res.end(xml_string);
-        });
-    }
+// サーバ機能の初期化
+let server = http.createServer();
+
+// リクエストがきたときに呼び出される86
+
+server.on('request', function(req, res)
+{
+    // 本文（Body部）に文字を表示する
+    res.write('Hello, world!');
+    res.end();
 });
-
-server.listen(3000);
+// 指定したIPアドレス、ポート番号でサーバを立てる
+server.listen(1337, '127.0.0.1');
