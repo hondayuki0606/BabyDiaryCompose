c174b8e016d4e13194704cf3931afb842ac86738
hondayuki0606 <hondayuki0606@yahoo.co.jp>
2024-09-06 22:22:35 +0900

    BDT-294, express-Validatorを修正

--- javascript-server/user/postBodyValidator.js
+++ javascript-server/user/postBodyValidator.js
@@ -1,6 +1,9 @@
-const { check } = require('express-validator');
+const { body } = require('express-validator');
 
 module.exports = [
-  check('itemId').not().isEmpty().withMessage('必須項目です。').isInt().withMessage('アイテムIDは数値を入力してください'),
-  check('itemName').not().isEmpty().withMessage('必須項目です。'),
+  body('user_name').not().isEmpty().withMessage('必須項目です。'),
+  body('first_name').not().isEmpty().withMessage('必須項目です。'),
+  body('last_name').not().isEmpty().withMessage('必須項目です。'),
+  body('password').not().isEmpty().withMessage('必須項目です。'),
+  body('user_status').not().isEmpty().withMessage('必須項目です。').isInt().withMessage('アイテムIDは数値を入力してください'),
 ];
